a33f81273ea0a3ea8a917d6df3b6f53c31e6e007
Ivan Dlugos <noreply+ivan@objectbox.io>
2020-11-06 15:53:38 +0100

    Store - add constructor from an existing C pointer to allow sharing store between multiple isolates

--- test/test_env.dart
+++ test/test_env.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:io';
 import 'entity.dart';
 import 'objectbox.g.dart';
@@ -14,8 +15,13 @@ class TestEnv {
     box = Box<TestEntity>(store);
   }
 
+  TestEnv.fromPtr(Pointer<Void> cStore) : dir = null {
+    store = Store.fromPtr(getObjectBoxModel(), cStore);
+    box = Box<TestEntity>(store);
+  }
+
   void close() {
     store.close();
-    if (dir.existsSync()) dir.deleteSync(recursive: true);
+    if (dir != null && dir.existsSync()) dir.deleteSync(recursive: true);
   }
 }
